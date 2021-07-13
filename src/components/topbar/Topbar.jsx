bae16d153ad1577b4585ea3e698242831512fdc1
Jasper Chang <hometree23@gmail.com>
2021-07-13 18:52:04 +0800

    Topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import "./topbar.css";
-import { Search } from '@material-ui/icons';
+import { Search, Person, Chat, Notifications } from '@material-ui/icons';
 
 export default function Topbar() {
     return (
@@ -12,9 +12,29 @@ export default function Topbar() {
             <div className="tipbarCenter">
                 <div className="searchbar">
                     <Search />
-                    <input type="text" />
-                </div></div>
-            <div className="topbarRight">3</div>
+                    <input type="text" className="searchInput" placeholder="搜尋您的朋友，或是發布文章、影片" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">HomePage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
