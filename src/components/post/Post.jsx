5b6b33f4d5b61f21c9298bd90758084ce7f6a6ca
Jasper Chang <hometree23@gmail.com>
2021-07-15 19:29:14 +0800

    post頁面ui完成

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -9,14 +9,26 @@ export default function Post() {
                     <div className="postTopLeft">
                         <img src="./assets/person/8.jpeg" alt="" className="postProfileImg" />
                         <span className="postUsername">Jasper</span>
-                        <span className="postDate">20210701</span>
+                        <span className="postDate">10分鐘前</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
                     </div>
                 </div>
-                <div className="postCenter"></div>
-                <div className="postButtom"></div>
+                <div className="postCenter">
+                    <span className="postText">我的第一po</span>
+                    <img src="./assets/post/1.jpeg" alt="" className="postImg" />
+                </div>
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img src="./assets/like.png" alt="" className="likeIcon" />
+                        <img src="./assets/heart.png" alt="" className="likeIcon" />
+                        <span className="postLikeCounter">23個人喜歡</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <div className="postCommentText">9 comments</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
