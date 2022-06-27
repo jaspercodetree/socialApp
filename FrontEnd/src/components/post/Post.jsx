203788d519a8d73d1b281d5cdf210d8e8acb9a17
Jasper Chang <hometree23@gmail.com>
2022-06-27 11:04:26 +0800

    feat: update share - emoji & tagUser
    1. add emoji modal on post
    2. search and tag user on post

--- FrontEnd/src/components/post/Post.jsx
+++ FrontEnd/src/components/post/Post.jsx
@@ -1,14 +1,18 @@
 import './post.css';
 // import { Users } from '../../dummyData';
 import { Link } from 'react-router-dom';
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState, useContext, useRef } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import axios from 'axios';
 //引入時間套件
 import { format } from 'timeago.js';
 import AlertDialog from '../alertDialog/AlertDialog';
+import CommentItem from '../commentItem/CommentItem';
+import { NearMeTwoTone } from '@material-ui/icons';
 
-export default function Post({ post }) {
+export default function Post({ originPost }) {
+	const [post, setPost] = useState(originPost);
+
 	//A.此處user與useEffect的getuser 是指去獲得  好友圈內每一則post貼文的發文者user
 	const [user, setUser] = useState({});
 
@@ -17,6 +21,11 @@ export default function Post({ post }) {
 	const { user: currentUser, PF } = useContext(AuthContext);
 	const [like, setLike] = useState(post.likes.length);
 	const [isLiked, setIsLiked] = useState(false);
+
+	const [writeComment, setWriteComment] = useState('');
+	const commentShareInput = useRef();
+
+	const [tagUserName, setTagUserName] = useState('');
 
 	//A.
 	useEffect(() => {
@@ -45,6 +54,73 @@ export default function Post({ post }) {
 		setIsLiked(!isLiked);
 	};
 
+	//get tagUserName
+	useEffect(() => {
+		const getTagUserName = async () => {
+			await axios
+				.get(`/users?userId=${post.tagUserId}`)
+				.then((res) => setTagUserName(res.data.username))
+				.catch((err) => console.log(err));
+		};
+		post.tagUserId && getTagUserName();
+	}, [post.tagUserId]);
+
+	//send comment
+	const sendComment = (e, isClick) => {
+		const sendCommentAjax = async () => {
+			try {
+				//generate guid
+				function _uuid() {
+					var d = Date.now();
+					if (
+						typeof performance !== 'undefined' &&
+						typeof performance.now === 'function'
+					) {
+						d += performance.now(); //use high-precision timer if available
+					}
+					return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+						/[xy]/g,
+						function (c) {
+							var r = (d + Math.random() * 16) % 16 | 0;
+							d = Math.floor(d / 16);
+							return (c === 'x' ? r : (r & 0x3) | 0x8).toString(
+								16
+							);
+						}
+					);
+				}
+				const _id = _uuid();
+
+				await axios
+					.put(`/posts/${post._id}/addComment`, {
+						comment: {
+							_id: _id,
+							commentUserId: currentUser._id,
+							commentUsername: currentUser.username,
+							commentProfilePic: currentUser.profilePicture,
+							commentText: writeComment,
+							commentCreatedAt: new Date(),
+						},
+					})
+					.then(async () => {
+						const res = await axios.get(`/posts/${post._id}`);
+						setPost(res.data);
+						setWriteComment('');
+					});
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
+		if (e.key === 'Enter' || isClick) {
+			writeComment && sendCommentAjax();
+
+			//禁止enter換行
+			e.preventDefault();
+			commentShareInput.current.value = '';
+		}
+	};
+
 	return (
 		<div className="post">
 			<div className="postWrapper">
@@ -62,13 +138,46 @@ export default function Post({ post }) {
 							/>
 						</Link>
 						<span className="postUsername">{user.username}</span>
-						<span className="postDate">
+						{post.emojiImg && (
+							<span className="emojiWrapper">
+								<span className="postEmojiImg">
+									正在
+									<img
+										className="mx-1 "
+										src={post.emojiImg}
+										alt=""
+									/>
+								</span>
+								<span>
+									覺得
+									<span className="postEmojiText fw-bold mx-1">
+										{post.emojiText}。
+									</span>
+								</span>
+							</span>
+						)}
+						{post.tagUserId && (
+							<span className="tagUserWrapper">
+								<span>
+									──和
+									<Link to={`/profile/${tagUserName}`}>
+										<span className="tagUserName fw-bold ms-2">
+											{tagUserName}
+										</span>
+									</Link>
+									。
+								</span>
+							</span>
+						)}
+						<span className="postDate ms-2">
 							{format(post.createdAt)}
 						</span>
 					</div>
-					<div className="postTopRight">
-						<AlertDialog post={post} />
-					</div>
+					{post.userId === currentUser._id && (
+						<div className="postTopRight">
+							<AlertDialog post={post} />
+						</div>
+					)}
 				</div>
 				<div className="postCenter">
 					<span className="postText">{post.desc}</span>
@@ -94,49 +203,67 @@ export default function Post({ post }) {
 						/>
 						<span className="postLikeCounter">{like}個人喜歡</span>
 					</div>
-					<div className="postBottomRight">
+					{/* <div className="postBottomRight">
 						<div className="postCommentText">留言</div>
-					</div>
+					</div> */}
 				</div>
 				<div className="postComment">
 					<div className="commentShare">
 						<div className="commentShareLeft">
 							<img
 								src={
-									user.profilePicture
-										? PF + `person/${user.profilePicture}`
+									currentUser.profilePicture
+										? PF +
+										  `person/${currentUser.profilePicture}`
 										: PF + `person/noAvatar.png`
 								}
 								alt=""
 								className="commentShareImg"
 							/>
 						</div>
-						<div className="commentShareRight">
-							<input
-								type="text"
+						<div className="commentShareRight px-3 py-2 position-relative">
+							<textarea
+								rows="1"
 								className="commentShareInput"
 								placeholder={`留言......`}
-							/>
+								onChange={(e) => {
+									setWriteComment(e.target.value);
+
+									//自適應高度
+									// console.log(
+									// 	e.target.style.height,
+									// 	e.target.scrollHeight
+									// );
+									e.target.style.height = 'auto';
+									e.target.style.height = `${e.target.scrollHeight}px`;
+								}}
+								onKeyDown={(e) => {
+									sendComment(e);
+								}}
+								ref={commentShareInput}
+							></textarea>
+							<button
+								type="button"
+								name="sendCommentBtn"
+								id="sendCommentBtn"
+								className="bg-transparent border-0 position-absolute end-0 me-4"
+								onClick={(e) => sendComment(e, true)}
+							>
+								<NearMeTwoTone />
+							</button>
 						</div>
 					</div>
 					<div className="commentList">
-						<div className="commentListItem">
-							<div className="commentListLeft">
-								<img
-									src={
-										user.profilePicture
-											? PF +
-											  `person/${user.profilePicture}`
-											: PF + `person/noAvatar.png`
-									}
-									alt=""
-									className="commentListImg"
+						{post.comments.map((comment) => {
+							return (
+								<CommentItem
+									key={comment._id}
+									comment={comment}
+									postId={post._id}
+									getNewPost={setPost}
 								/>
-							</div>
-							<div className="commentListRight">
-								<h5 className="commentListItemText">讚啦</h5>
-							</div>
-						</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
--- FrontEnd/src/components/share/Share.jsx
+++ FrontEnd/src/components/share/Share.jsx
@@ -5,24 +5,74 @@ import {
 	Room,
 	EmojiEmotions,
 	Cancel,
+	Search,
 } from '@material-ui/icons';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { useRef } from 'react';
 import axios from 'axios';
+import { Modal } from 'react-bootstrap';
+import SearchTagUser from '../searchTagUser/SearchTagUser';
+import { Link } from 'react-router-dom';
 
 export default function Share() {
 	const { user, PF } = useContext(AuthContext);
 	const desc = useRef();
 	const [file, setFile] = useState(null);
 
+	const [emojiModalShow, setEmojiModalShow] = useState(false);
+
+	let [newPost, setNewPost] = useState({ userId: user._id });
+
+	const [tagModalShow, setTagModalShow] = useState(false);
+	const [searchName, setSearchName] = useState([]);
+	const [searchTagUsers, setSearchTagUsers] = useState([]);
+	const [allUsers, setAllUsers] = useState([]);
+	const [tagUserName, setTagUserName] = useState('');
+	//searchTagUsers
+	useEffect(() => {
+		if (searchName.length !== 0) {
+			const getUsers = async () => {
+				await axios
+					.get(`/users/search?username=${searchName}`)
+					.then((res) => setSearchTagUsers(res.data))
+					.catch((err) => console.log(err));
+			};
+			getUsers();
+		}
+	}, [searchName]);
+
+	//get all users except master
+	useEffect(() => {
+		const getAllUsers = async () => {
+			await axios
+				.get(`/users/all`)
+				.then((res) => {
+					const usersExceptMaster = res.data.filter(
+						(item) => item._id !== user._id
+					);
+					setAllUsers(usersExceptMaster.slice(0, 5));
+				})
+				.catch((err) => console.log(err));
+		};
+		getAllUsers();
+	}, [user._id]);
+
+	//get tagUserName
+	useEffect(() => {
+		const getTagUserName = async () => {
+			await axios
+				.get(`/users?userId=${newPost.tagUserId}`)
+				.then((res) => setTagUserName(res.data.username))
+				.catch((err) => console.log(err));
+		};
+		newPost.tagUserId && getTagUserName();
+	}, [newPost.tagUserId]);
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
-		const newPost = {
-			userId: user._id,
-			desc: desc.current.value,
-		};
+		console.log(newPost);
 
 		//1.透過/upload將圖片上傳到資料夾，並將路徑名稱存到newPost；2.再將newPost 透過/posts上傳到資料庫
 		if (file) {
@@ -30,8 +80,10 @@ export default function Share() {
 			const filename = Date.now() + file.name;
 			data.append('name', filename);
 			data.append('file', file);
+
 			newPost.img = filename;
 			console.log(newPost);
+
 			try {
 				await axios.post('/upload', data);
 			} catch (error) {
@@ -47,6 +99,37 @@ export default function Share() {
 		}
 	};
 
+	//emoji
+	function getEmoji(e) {
+		setNewPost({
+			...newPost,
+			emojiImg: e.target.querySelector('img').src,
+			emojiText: e.target.querySelector('span').textContent,
+		});
+		setEmojiModalShow(false);
+		// console.log(newPost);
+		// console.log(e.target.querySelector('img').src);
+		// console.log(e.target.querySelector('span').textContent);
+		// if (e.target !== this) return false;
+	}
+
+	//location
+	let latitude = '';
+	let longitude = '';
+	function getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			console.log('Geolocation is not supported by this browser.');
+		}
+	}
+
+	function showPosition(position) {
+		latitude = position.coords.latitude;
+		longitude = position.coords.longitude;
+		console.log(latitude, longitude);
+	}
+
 	return (
 		<div className="share">
 			<form className="shareWrapper" onSubmit={handleSubmit}>
@@ -60,11 +143,75 @@ export default function Share() {
 						alt=""
 						className="shareProfileImg"
 					/>
-					<input
-						placeholder={`${user.username} 今天coding了嗎?`}
-						className="shareInput"
-						ref={desc}
-					/>
+					<div className="d-flex flex-column w-100">
+						<div>
+							{(newPost.emojiImg || newPost.tagUserId) && (
+								<>
+									<span className="postUsername">
+										{user.username}
+									</span>
+								</>
+							)}
+							{newPost.emojiImg && (
+								<>
+									<span className="emojiWrapper">
+										<span className="postEmojiImg">
+											正在
+											<img
+												className="mx-1 "
+												src={newPost.emojiImg}
+												alt=""
+											/>
+										</span>
+										<span>
+											覺得
+											<span className="postEmojiText fw-bold mx-1">
+												{newPost.emojiText}。
+											</span>
+										</span>
+									</span>
+								</>
+							)}
+							{newPost.tagUserId && (
+								<span className="tagUserWrapper">
+									<span>
+										──和
+										<Link to={`/profile/${tagUserName}`}>
+											<span className="tagUserName fw-bold ms-2 text-black">
+												{tagUserName}
+											</span>
+										</Link>
+										。
+									</span>
+								</span>
+							)}
+						</div>
+						<div className="sharePostWrapper py-2 position-relative">
+							<textarea
+								rows="1"
+								className="sharePostInput d-block"
+								placeholder={`${user.username} 今天coding了嗎 ?`}
+								onChange={(e) => {
+									setNewPost({
+										...newPost,
+										desc: desc.current.value,
+									});
+
+									//自適應高度
+									e.target.style.height = 'auto';
+									e.target.style.height = `${e.target.scrollHeight}px`;
+								}}
+								// onKeyDown={(e) => {
+								// 	setNewPost({
+								// 		...newPost,
+								// 		userId: user._id,
+								// 		desc: desc.current.value,
+								// 	});
+								// }}
+								ref={desc}
+							></textarea>
+						</div>
+					</div>
 				</div>
 				<hr className="shareHr" />
 				{/* 上傳圖片預覽 */}
@@ -89,7 +236,7 @@ export default function Share() {
 								htmlColor="tomato"
 								className="shareIcon"
 							/>
-							<span className="shareOptionText">照片</span>
+							{/* <span className="shareOptionText">照片</span> */}
 							<input
 								style={{ display: 'none' }}
 								type="file"
@@ -99,20 +246,28 @@ export default function Share() {
 								onChange={(e) => setFile(e.target.files[0])}
 							/>
 						</label>
-						<div className="shareOption">
+						<div
+							className="shareOption"
+							onClick={() => setTagModalShow(true)}
+						>
 							<Label htmlColor="green" className="shareIcon" />
-							<span className="shareOptionText">標籤</span>
+							{/* <span className="shareOptionText">標註</span> */}
 						</div>
-						<div className="shareOption">
+						<div className="shareOption" onClick={getLocation}>
 							<Room htmlColor="blue" className="shareIcon" />
-							<span className="shareOptionText">位置</span>
+							{/* <span className="shareOptionText">位置</span> */}
 						</div>
-						<div className="shareOption">
+						<div
+							className="shareOption"
+							onClick={() => setEmojiModalShow(true)}
+						>
 							<EmojiEmotions
 								htmlColor="orange"
 								className="shareIcon"
 							/>
-							<span className="shareOptionText">心情</span>
+							{/* <span className="shareOptionText" variant="primary">
+								心情
+							</span> */}
 						</div>
 					</div>
 					<button className="shareButton" type="submit">
@@ -120,6 +275,189 @@ export default function Share() {
 					</button>
 				</div>
 			</form>
+
+			<Modal
+				className="emojiModal"
+				show={emojiModalShow}
+				onHide={() => setEmojiModalShow(false)}
+				dialogClassName="modal-90w"
+				aria-labelledby="example-custom-modal-styling-title"
+				centered={true}
+			>
+				<Modal.Header closeButton>
+					<Modal.Title
+						id="example-custom-modal-styling-title"
+						className="fs-5 "
+					>
+						你現在感受如何？
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body className="container">
+					<ul className="row p-0 m-0">
+						{[
+							{ img: '10065', text: '開心' },
+							{ img: '10001', text: '被祝福' },
+							{ img: '10035', text: '感恩' },
+							{ img: '10089', text: '充滿希望' },
+							{ img: '10001', text: '被愛' },
+							{ img: '10052', text: '興奮' },
+							{ img: '10001', text: '戀愛 ing' },
+							{ img: '10002', text: '抓狂' },
+							{ img: '10027', text: '悲傷' },
+							{ img: '10088', text: '幸運' },
+							{ img: '10001', text: '可愛' },
+							{ img: '10093', text: '幸福' },
+							{ img: '10022', text: '傻呼呼' },
+							{ img: '10023', text: '好夢幻' },
+							{ img: '10014', text: '超人' },
+							{ img: '10090', text: '喜氣洋洋' },
+						].map((item) => (
+							<li
+								key={item.text}
+								className="col-6"
+								onClick={getEmoji}
+							>
+								<div className="box d-flex justify-content-start align-items-center p-2">
+									<div
+										className="emojiItem"
+										onClick={(e) => {
+											e.stopPropagation();
+											setNewPost({
+												...newPost,
+												emojiImg:
+													e.target.parentNode.querySelector(
+														'img'
+													).src,
+												emojiText:
+													e.target.parentNode.querySelector(
+														'span'
+													).textContent,
+											});
+											setEmojiModalShow(false);
+										}}
+									>
+										<img
+											src={PF + `emoji/${item.img}.png`}
+											alt=""
+											onClick={(e) => {
+												e.stopPropagation();
+												setNewPost({
+													...newPost,
+													emojiImg:
+														e.target.parentNode.querySelector(
+															'img'
+														).src,
+													emojiText:
+														e.target.parentNode.parentNode.querySelector(
+															'span'
+														).textContent,
+												});
+												setEmojiModalShow(false);
+											}}
+										/>
+									</div>
+									<div
+										className="textItem"
+										onClick={(e) => {
+											e.stopPropagation();
+											setNewPost({
+												...newPost,
+												emojiImg:
+													e.target.parentNode.querySelector(
+														'img'
+													).src,
+												emojiText:
+													e.target.parentNode.querySelector(
+														'span'
+													).textContent,
+											});
+											setEmojiModalShow(false);
+										}}
+									>
+										<span
+											onClick={(e) => {
+												e.stopPropagation();
+												setNewPost({
+													...newPost,
+													emojiImg:
+														e.target.parentNode.parentNode.querySelector(
+															'img'
+														).src,
+													emojiText:
+														e.target.parentNode.parentNode.querySelector(
+															'span'
+														).textContent,
+												});
+												setEmojiModalShow(false);
+											}}
+										>
+											{item.text}
+										</span>
+									</div>
+								</div>
+							</li>
+						))}
+					</ul>
+				</Modal.Body>
+			</Modal>
+
+			<Modal
+				className="tagModal"
+				show={tagModalShow}
+				onHide={() => setTagModalShow(false)}
+				dialogClassName="modal-90w"
+				centered={true}
+			>
+				<Modal.Header closeButton>
+					<Modal.Title
+						id="example-custom-modal-styling-title"
+						className="fs-5 "
+					>
+						標註用戶
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body className="container">
+					<div className="searchBar">
+						<Search className="searchIcon" />
+						<input
+							type="text"
+							className="searchInput bg-transparent"
+							onChange={(e) => setSearchName(e.target.value)}
+							placeholder={`搜尋使用者`}
+						/>
+					</div>
+					<h6 className={`pt-3 pb-1 px-3 m-0 `}>
+						{`${searchName.length === 0 ? '推薦用戶' : '搜尋結果'}`}
+					</h6>
+					<div
+						className={`searchTagUserListWrap bg-white pb-3 mt-1border border-gray rounded-3`}
+					>
+						<ul className="searchTagUserList m-0 p-0">
+							{searchName.length === 0
+								? allUsers.map((u) => (
+										<SearchTagUser
+											className="text-black"
+											key={u._id}
+											user={u}
+											setNewPost={setNewPost}
+											newPost={newPost}
+											setTagModalShow={setTagModalShow}
+										/>
+								  ))
+								: searchTagUsers.map((u) => (
+										<SearchTagUser
+											className="text-black"
+											key={u._id}
+											user={u}
+											setNewPost={setNewPost}
+											newPost={newPost}
+											setTagModalShow={setTagModalShow}
+										/>
+								  ))}
+						</ul>
+					</div>
+				</Modal.Body>
+			</Modal>
 		</div>
 	);
 }
