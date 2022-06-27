295d94b2902e81e226eb294ca2c5b0b3923771f9
Jasper Chang <hometree23@gmail.com>
2022-06-27 11:35:06 +0800

    fix: rename files - 1.rightBar 2. topBar

---
+++ FrontEnd/src/components/rightBar/RightBar.jsx
@@ -0,0 +1,178 @@
+import './rightBar.css';
+import Advertisement from '../advertisement/Advertisement';
+import { Add, Remove } from '@material-ui/icons';
+import Online from '../online/Online';
+import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
+
+export default function RightBar({ user, isHomePage }) {
+	const [friends, setFriends] = useState([]);
+	const [profileFriends, setProfileFriends] = useState([]);
+	const { user: loginUser, dispatch, PF } = useContext(AuthContext);
+
+	const [followed, setFollowed] = useState(false);
+
+	// console.log(user);
+	/* user為此profile頁面的user ;  loginUser為目前登入的user */
+
+	//profile 線上朋友列表
+	useEffect(() => {
+		if (loginUser && Object.entries(loginUser).length !== 0) {
+			// console.log(Object.entries(loginUser));
+			const getFriends = async () => {
+				await axios
+					.get('/users/friends/' + loginUser._id)
+					.then((res) => setFriends(res.data))
+					.catch((err) => console.log(err));
+			};
+			getFriends();
+		}
+	}, [loginUser]);
+
+	//profile 右側欄朋友列表
+	useEffect(() => {
+		if (user && Object.entries(user).length !== 0) {
+			// console.log(Object.entries(user));
+			const getProfileFriends = async () => {
+				await axios
+					.get(`/users/friends/` + user._id)
+					.then((res) => {
+						res.data.length !== 0
+							? setProfileFriends(res.data)
+							: setProfileFriends([
+									{
+										profilePicture: 'noAvatar.png',
+										username: 'QwQ 朋友募集中',
+									},
+							  ]);
+					})
+					.catch((err) => console.log(err));
+			};
+			getProfileFriends();
+		}
+	}, [user]);
+
+	//加入/解除追蹤
+	useEffect(() => {
+		setFollowed(loginUser.followings.includes(user?._id));
+	}, [loginUser, user?._id]);
+
+	//注意:即使已經更新資料庫，但由於追蹤按鈕的文字是由context內的followings是否有user._id判斷，因此我們必須透過useReducer去更新context的狀態，來更新文字
+	const handleClick = async () => {
+		if (followed) {
+			await axios.put(`/users/${user._id}/unfollow`, {
+				userId: loginUser._id,
+			});
+			dispatch({ type: 'UNFOLLOW', payload: user._id });
+		} else {
+			await axios.put(`/users/${user._id}/follow`, {
+				userId: loginUser._id,
+			});
+			dispatch({ type: 'FOLLOW', payload: user._id });
+		}
+
+		// setFollowed(!followed);
+	};
+
+	const HomeRightBar = () => {
+		return (
+			<>
+				<div className="birthdayContainer">
+					<img src="assets/gift.png" alt="" className="birthdayImg" />
+					<span className="birthdayText">
+						<b>曾國峰</b> 以及 <b>5個其他的朋友</b> 今天生日
+					</span>
+				</div>
+				<Advertisement />
+				<div className="rightBarTitle">在線的朋友</div>
+				<ul className="rightBarFriendList">
+					{friends.map((f) => (
+						<Online key={f._id} friend={f} />
+					))}
+				</ul>
+			</>
+		);
+	};
+
+	const ProfileRightBar = () => {
+		return (
+			<>
+				{loginUser.username !== user.username && (
+					<div className="rightBarFollowButton" onClick={handleClick}>
+						{followed ? '解除追蹤 ' : '加入追蹤 '}
+						{followed ? <Remove /> : <Add />}
+					</div>
+				)}
+
+				<h4 className="rightBarTitle">使用者資訊</h4>
+				<div className="rightBarInfo">
+					<div className="rightBarInfoItem">
+						<span className="rightBarInfoKey">城市:</span>
+						<span className="rightBarInfoValue">
+							{user.city ? user.city : '神秘都市'}
+						</span>
+					</div>
+					<div className="rightBarInfoItem">
+						<span className="rightBarInfoKey">家鄉:</span>
+						<span className="rightBarInfoValue">
+							{user.from ? user.from : '秘密之地'}
+						</span>
+					</div>
+					<div className="rightBarInfoItem">
+						<span className="rightBarInfoKey">關係:</span>
+						<span className="rightBarInfoValue">
+							{user.relationship === 0
+								? '單身'
+								: user.relationship === 1
+								? '已婚'
+								: '秘密'}
+						</span>
+					</div>
+				</div>
+
+				<h4 className="rightBarTitle">使用者朋友</h4>
+				<div className="rightBarFollowings">
+					{profileFriends.map((friend) => (
+						<Link
+							to={`/profile/${friend.username}`}
+							style={{ textDecoration: 'none' }}
+							key={friend._id}
+						>
+							<div className="rightBarFollowing">
+								<img
+									src={
+										friend.profilePicture
+											? PF +
+											  `person/${friend.profilePicture}`
+											: PF + '/person/noAvatar.png'
+									}
+									alt=""
+									className="rightBarFollowingImg"
+								/>
+								<span className="rightBarFollowingName">
+									{friend.username}
+								</span>
+							</div>
+						</Link>
+					))}
+				</div>
+				<Advertisement />
+			</>
+		);
+	};
+	return (
+		<div
+			className={
+				isHomePage === true
+					? 'rightBar d-none d-md-block col-md-3 col-lg-2'
+					: 'rightBar d-none d-md-block col-md-3'
+			}
+		>
+			<div className="rightBarWrapper">
+				{user ? <ProfileRightBar /> : <HomeRightBar />}
+			</div>
+		</div>
+	);
+}
---
+++ FrontEnd/src/components/topBar/TopBar.jsx
@@ -0,0 +1,108 @@
+import './topBar.css';
+import {
+	Search,
+	Person,
+	Chat,
+	Notifications,
+	ExitToAppSharp,
+} from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import { useContext, useState, useEffect } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import SearchUser from '../searchUser/SearchUser';
+import axios from 'axios';
+
+export default function TopBar() {
+	const { user, PF } = useContext(AuthContext);
+	const [searchName, setSearchName] = useState([]);
+	const [searchUsers, setSearchUsers] = useState([]);
+
+	const logout = () => {
+		localStorage.removeItem('user');
+		window.location.replace('/login');
+	};
+
+	//searchUsers
+	useEffect(() => {
+		if (searchName.length !== 0) {
+			const getUsers = async () => {
+				await axios
+					.get(`/users/search?username=${searchName}`)
+					.then((res) => setSearchUsers(res.data))
+					.catch((err) => console.log(err));
+			};
+			getUsers();
+		}
+	}, [searchName]);
+
+	return (
+		<div className="topBarContainer container-fluid">
+			<div className="row">
+				<div className="topBarLeft col-4 col-lg-4">
+					<Link to="/" style={{ textDecoration: 'none' }}>
+						<span className="logo">JasperBook</span>
+					</Link>
+				</div>
+				<div className="topBarCenter col-5 col-lg-4">
+					<div className="searchBar">
+						<Search className="searchIcon" />
+						<input
+							type="text"
+							className="searchInput bg-transparent"
+							onChange={(e) => setSearchName(e.target.value)}
+							placeholder={`搜尋用戶`}
+						/>
+					</div>
+					<div
+						className={`searchUserListWrap position-absolute bg-white py-3 mt-1 border border-gray rounded-3 ${
+							searchName.length === 0 ? 'd-none' : ''
+						}`}
+					>
+						<ul className="searchUserList m-0 p-0">
+							{searchUsers.map((u) => (
+								<Link
+									to={`/profile/${u.username}`}
+									style={{ textDecoration: 'none' }}
+									className="text-black"
+									key={u._id}
+								>
+									<SearchUser user={u} />
+								</Link>
+							))}
+						</ul>
+					</div>
+				</div>
+				<div className="topBarRight col-3 col-lg-4">
+					<Link to={`/profile/${user.username}`}>
+						<img
+							src={
+								user.profilePicture
+									? PF + `/person/${user.profilePicture}`
+									: PF + '/person/noAvatar.png'
+							}
+							alt="userAvatar"
+							className="topBarImg me-3"
+						/>
+					</Link>
+					<div className="topBarIcons">
+						{/* <div className="topBarIconItem">
+							<Person />
+							<span className="topBarIconBadge">1</span>
+						</div>
+						<div className="topBarIconItem">
+							<Chat />
+							<span className="topBarIconBadge">2</span>
+						</div>
+						<div className="topBarIconItem">
+							<Notifications />
+							<span className="topBarIconBadge">3</span>
+						</div> */}
+						<div className="topBarIconItem" onClick={logout}>
+							<ExitToAppSharp />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
